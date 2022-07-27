f5ce94f03a4635289cf03ad595785c8b9cf547df
Aaron Traas <aaron@traas.org>
2022-07-27 10:43:59 -0400

    Initial stub of plugin

---
+++ disneyplus_keybind.js
@@ -0,0 +1,31 @@
+console.debug('[d+keybind] *** Keybindings for D+ Loaded ***');
+
+console.debug('[d+keybind] grabbing active tab');
+
+try {
+
+	const stylesheetUrl = browser.extension.getURL("disneyplus_keybind.css");
+	console.debug('[d+keybind] URL for CSS to insert: ' + stylesheetUrl);
+
+	var styleElement = document.createElement('link');
+	styleElement.type = 'text/css';
+	styleElement.rel = 'stylesheet';
+	styleElement.href = stylesheetUrl;
+	console.debug('[d+keybind] style element: ', styleElement);
+
+	document.head.appendChild(styleElement);
+	console.debug('[d+keybind] style element appended');
+
+	/*
+	let lists = document.querySelectorAll('slick-list');
+
+	activeList = lists[0];
+
+	document.addEventListener('keydown', (event) => {
+		console.debug('Key down: ' + event.code);
+	});
+	*/
+} catch (err) {
+	log.debug(err);
+}
+
